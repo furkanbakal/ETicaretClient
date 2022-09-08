bd2851d714ec69be2ba2c47e9d5c3982f12c08f4
furkanbakal <f.bakal1997@gmail.com>
2022-09-08 23:44:04 +0300

    spinner cuıstomized

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,9 +9,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToastrModule } from 'ngx-toastr';
 import { NgxSpinnerModule } from "ngx-spinner";
 
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
